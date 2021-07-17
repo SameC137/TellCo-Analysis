32e5912cd4b66335de3c279888bac2c40a389e68
SameC137 <samemichael1415@gmail.com>
2021-07-17 20:25:41 +0300

    modified some files, added helper functions to separate file

--- scripts/extract_data.py
+++ scripts/extract_data.py
@@ -1,1 +1,9 @@
 
+import pandas as pd
+def drop_duplicate(self, df:pd.DataFrame,subset:list)->pd.DataFrame:
+    """
+    drop duplicate rows
+    """
+    return df.drop_duplicates(subset=subset, keep=False,inplace=false)
+
+    
